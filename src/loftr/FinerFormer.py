52eaf374efbe14df6b0aa6960ff76f3a214f6c0f
Dongyue Li <41225553+HUSTNO1WXY@users.noreply.github.com>
2024-06-30 10:30:37 +0800

    Update and rename loftr.py to FinerFormer.py

--- src/loftr/FinerFormer.py
+++ src/loftr/FinerFormer.py
@@ -16,7 +16,7 @@ from .utils.coarse_matching import CoarseMatching
 from .utils.fine_matching import FineMatching
 
 
-class LoFTR(nn.Module):
+class FineFormer(nn.Module):
     def __init__(self, config):
         super().__init__()
         # Misc
